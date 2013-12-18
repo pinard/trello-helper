83075f9a7a94b790c44f539544ea498df92b3f58
François Pinard <pinard@iro.umontreal.ca>
2013-12-18 00:11:43 -0500

    First attempt at implementing trello-helper-to-clipboard, which is the Org markup to Markdown counterpart.

--- trello-helper.el
+++ trello-helper.el
@@ -1,6 +1,9 @@
 ;;;; Bits for handling Org files produced by trello-helper.
 ;;;; See https://github.com/pinard/trello-helper for details.
 
+(defvar trello-helper-command "trello-helper"
+  "Name, or possibly path, or the trello-helper script")
+
 (defun trello-helper-org-mode-hook ()
   ;; Hide inline information about Trello ids.  If you temporarily
   ;; need to see it, toggle using the M-x visible-mode command.
@@ -10,3 +13,12 @@
                                   '(invisible org-link)))))))
 
 (add-hook 'org-mode-hook 'trello-helper-org-mode-hook)
+
+(defun trello-helper-to-clipboard (start end)
+  ;; Copy region to clipboard, rewriting it to Markdown while doing so.
+  (when (equal start end)
+    (error "Please select a region first!"))
+  (interactive "r")
+  (shell-command-on-region
+   start end (concat trello-helper-command " -m")))
+
