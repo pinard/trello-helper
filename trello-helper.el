63ff2611dd6e25eb92f70d1c25d9c6a26ff2fc5f
François Pinard <pinard@iro.umontreal.ca>
2014-01-03 00:10:33 -0500

    trello-helper.el: Implement org-trello-mode-hook.

--- trello-helper.el
+++ trello-helper.el
@@ -4,15 +4,36 @@
 (defvar trello-helper-command "trello-helper"
   "Name, or possibly path, or the trello-helper script")
 
-(defun trello-helper-org-mode-hook ()
+(defvar trello-helper-mode-hook nil
+  "List of hooks to run on trello-helper Org files.")
+
+(defconst trello-helper-inline-id-regexp
+  " *:PROPERTIES: {\"orgtrello-id\":[^}\n]+}")
+
+(defconst trello-helper-propery-id-regexp
+  "^:orgtrello-id:")
+
+(defun trello-helper-hook-runner ()
+  (when (save-excursion
+          (goto-char (point-min))
+          (re-search-forward
+           (concat trello-helper-inline-id-regexp
+                   "\\|"
+                   trello-helper-propery-id-regexp)
+           nil t))
+    (run-hooks 'trello-helper-mode-hook)))
+
+(add-hook 'org-mode-hook 'trello-helper-hook-runner)
+
+(defun trello-helper-hide-trello-ids ()
   ;; Hide inline information about Trello ids.  If you temporarily
   ;; need to see it, toggle using the M-x visible-mode command.
   (font-lock-add-keywords
-   nil '((" *:PROPERTIES: {\"orgtrello-id\":[^}\n]+}"
+   nil `((,trello-helper-inline-id-regexp
           (0 (add-text-properties (match-beginning 0) (match-end 0)
                                   '(invisible org-link)))))))
 
-(add-hook 'org-mode-hook 'trello-helper-org-mode-hook)
+(add-hook 'trello-helper-mode-hook 'trello-helper-hide-trello-ids)
 
 (defun trello-helper-to-clipboard (start end)
   ;; Copy region to clipboard, rewriting it to Markdown while doing so.
